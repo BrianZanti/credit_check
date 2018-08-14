23d25048662320a822087b975a595e0c2a7e4fb1
Sal Espinosa <sespinos@gmail.com>
2018-08-14 17:51:56 -0600

    Put algorithm into methods

--- lib/credit_check.rb
+++ lib/credit_check.rb
@@ -1,44 +1,61 @@
 require 'pry'
 
-card_number = "5541808923795240"
-digits = card_number.chars
-integers = []
-digits.each do |digit|
-  # integers.push digit.to_i
-  integers << digit.to_i
+def is_valid?(card_number)
+  integers        = get_digits(card_number)
+  doubled         = double_integers(integers)
+  summed_over_ten = sum_doubled(doubled)
+  sum             = sum_digits(summed_over_ten)
+  print_validity(sum, card_number)
 end
 
-doubled = []
-integers.each.with_index do |integer, index|
-  if index.even?
-    doubled << integer * 2
+def get_digits(card_number)
+  digits = card_number.chars
+  integers = []
+  digits.each do |digit|
+    integers << digit.to_i
+  end
+  return integers
+end
+
+def double_integers(integers)
+  doubled = []
+  integers.each.with_index do |integer, index|
+    if index.even?
+      doubled << integer * 2
+    else
+      doubled << integer
+    end
+  end
+  return doubled
+end
+
+def sum_doubled(doubled)
+  summed_over_ten = []
+  doubled.each do |digit|
+    if digit > 9
+      summed_over_ten << digit - 9
+    else
+      summed_over_ten << digit
+    end
+  end
+  summed_over_ten
+end
+
+def sum_digits(summed_over_ten)
+  sum = 0
+  summed_over_ten.each do |digit|
+    sum += digit
+  end
+  sum
+end
+
+def print_validity(sum, card_number)
+  if sum % 10 == 0
+    puts "the card #{card_number} is valid"
   else
-    doubled << integer
+    puts "the card #{card_number} is invalid"
   end
 end
 
-summed_over_ten = []
-doubled.each do |digit|
-  if digit > 9
-    summed_over_ten << digit - 9
-  else
-    summed_over_ten << digit
-  end
-end
-
-sum = 0
-summed_over_ten.each do |digit|
-  sum += digit
-end
-
-if sum % 10 == 0
-  puts "the card #{card_number} is valid"
-else
-  puts "the card #{card_number} is invalid"
-end
-
-# Your Luhn Algorithm Here
-
-# Output
-## If it is valid, print "The number [card number] is valid!"
-## If it is invalid, print "The number [card number] is invalid!"
+is_valid?("5541808923795240")
+is_valid?("5541808923795241")
