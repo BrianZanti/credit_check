12ad81f99886956c2abeacd18232f9e144ee11b9
Brian Zanti <brianzanti@gmail.com>
2018-08-14 17:14:17 -0600

    adds credit check algorithm

--- lib/credit_check.rb
+++ lib/credit_check.rb
@@ -1,27 +1,41 @@
 require 'pry'
 
-def convert_to_int
-  card_number = "5541808923795240"
-
-  digits = card_number.chars
-
-  integers = []
-
-  digits.each do |digit|
-    # integers.push digit.to_i
-    integers << digit.to_i
-  end
+card_number = "5541808923795240"
+digits = card_number.chars
+integers = []
+digits.each do |digit|
+  # integers.push digit.to_i
+  integers << digit.to_i
 end
 
 doubled = []
-
-integers.each do |integer|
-  doubled << integer * 2
+integers.each.with_index do |integer, index|
+  if index.even?
+    doubled << integer * 2
+  else
+    doubled << integer
+  end
 end
 
-p doubled
+summed_over_ten = []
+doubled.each do |digit|
+  if digit > 9
+    summed_over_ten << digit - 9
+  else
+    summed_over_ten << digit
+  end
+end
 
+sum = 0
+summed_over_ten.each do |digit|
+  sum += digit
+end
 
+if sum % 10 == 0
+  puts "the card #{card_number} is valid"
+else
+  puts "the card #{card_number} is invalid"
+end
 
 # Your Luhn Algorithm Here
 
