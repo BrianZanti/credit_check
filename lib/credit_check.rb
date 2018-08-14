e84056368d0311c4c65ca16e32af74919a227676
Brian Zanti <brianzanti@gmail.com>
2018-08-14 11:36:29 -0600

    each

--- lib/credit_check.rb
+++ lib/credit_check.rb
@@ -1,4 +1,27 @@
-card_number = "5541808923795240"
+require 'pry'
+
+def convert_to_int
+  card_number = "5541808923795240"
+
+  digits = card_number.chars
+
+  integers = []
+
+  digits.each do |digit|
+    # integers.push digit.to_i
+    integers << digit.to_i
+  end
+end
+
+doubled = []
+
+integers.each do |integer|
+  doubled << integer * 2
+end
+
+p doubled
+
+
 
 # Your Luhn Algorithm Here
 
